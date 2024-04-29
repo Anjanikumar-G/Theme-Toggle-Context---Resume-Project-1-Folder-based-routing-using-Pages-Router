5924b5298a57ffae00dc82d2d9f43966543855fe
Anjanikumar Guruvali <108091230+Anjanikumar-G@users.noreply.github.com>
2024-04-29 15:57:16 +0530

    Update ThemeToggleButton.js

--- app/components/ThemeToggleButton.js
+++ app/components/ThemeToggleButton.js
@@ -1,14 +1,20 @@
 'use client'
-import React from 'react';
-
+import React, { useContext } from 'react';
+import { ThemeContext } from './ThemeProvider';
 
 const ThemeToggleButton = () => {
-    
-    return (
-       <>
-       
-       </>
-    )
+    // Access theme and toggleTheme function from the context
+    const { theme, toggleTheme } = useContext(ThemeContext);
 
+    return (
+        <button
+            id="global-theme-toggler"
+            className={`btn ${theme === 'light' ? 'btn-light txt-light' : 'btn-dark txt-dark'}`}
+            onClick={toggleTheme}
+        >
+            {theme === "light" ? "Switch to dark theme" : "Switch to light theme"}
+        </button>
+    );
 }
-export {ThemeToggleButton}
+
+export { ThemeToggleButton };
