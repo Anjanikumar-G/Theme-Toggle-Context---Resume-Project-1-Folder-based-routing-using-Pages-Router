e2afd900d1d0beb7ff5aa1b3f0e3faa848debb98
Anjanikumar Guruvali <108091230+Anjanikumar-G@users.noreply.github.com>
2024-05-04 12:36:25 +0530

    Update LocalThemedBox.js

--- app/components/LocalThemedBox.js
+++ app/components/LocalThemedBox.js
@@ -1,30 +1,22 @@
-'use client'
-import React, { useContext } from 'react';
+import React from 'react';
 import { ThemeContext } from './ThemeProvider';
 
 const LocalThemedBox = () => {
-    // Access theme and toggleTheme function from the context
-    const { theme, toggleTheme } = useContext(ThemeContext);
-
-    // Function to toggle the local theme
-    const toggleLocalTheme = () => {
-        // Implement logic to toggle the local theme
-    };
-
-    return (
-        <div style={{ width: '200px', height: '200px', border: '2px solid green' }} id="local-themed-box" className={`bg-${theme}`}>
-            <p id="local-themed-text-container" className={`txt-${theme}`}>
-                Local Themed Text
-            </p>
-            <button
-                id="local-theme-toggler"
-                className={`btn ${theme === 'light' ? 'btn-light' : 'btn-dark'}`}
-                onClick={toggleLocalTheme}
-            >
-                {theme === "light" ? "Toggle local theme to dark" : "Toggle local theme to light"}
-            </button>
-        </div>
-    );
+    const {theme} = React.useContext(ThemeContext)
+    const [localTheme,setLocalTheme] = React.useState(theme)
+    React.useEffect(()=>{
+        setLocalTheme(theme)
+    },[theme])
+return(
+    <div style={{width:'200px',height:'200px',border:'2px solid green'}} id="local-themed-box" className={`bg-${localTheme}`}>
+        <p className={`txt-${localTheme}`} id="local-themed-text-container">
+            Hiiii
+        </p>
+        <button id="local-theme-toggler" className={`btn btn-${localTheme}`}onClick={()=>{setLocalTheme(localTheme === 'light' ? 'dark' : 'light')}}>
+            Toggle local theme to {localTheme === 'light' ? 'dark' : 'light'}
+        </button>
+    </div>
+)
 }
 
-export { LocalThemedBox };
+export { LocalThemedBox }
